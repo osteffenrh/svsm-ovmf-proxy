6b48c755d446e791fa3d057d4f6b57d405eb2a70
Oliver Steffen <osteffen@redhat.com>
2024-11-14 04:41:52 -0500

    build.sh: Build correct OVMF

--- build.sh
+++ build.sh
@@ -12,9 +12,9 @@ pushd edk2
 git submodule update --init --depth=1 --single-branch
 make -C BaseTools -j$(nproc)
 . ./edksetup.sh
-build -a X64 -b DEBUG -t GCC5 -D DEBUG_ON_SERIAL_PORT -D DEBUG_VERBOSE -DTPM2_ENABLE -p OvmfPkg/AmdSev/AmdSevX64.dsc \
+build -a X64 -b DEBUG -t GCC5 -D DEBUG_ON_SERIAL_PORT -D DEBUG_VERBOSE -DTPM2_ENABLE -p OvmfPkg/OvmfPkgX64.dsc \
   -Y COMPILE_INFO -y BuildReport.log 2>&1 | tee Build.log
-cp Build/AmdSev/DEBUG_GCC5/FV/OVMF.fd ..
+cp Build/OvmfX64/DEBUG_GCC5/FV/OVMF.fd ..
 popd
 
 pushd svsm
